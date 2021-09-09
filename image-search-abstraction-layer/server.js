8fb9e9b7c6098839bb1ef82be4d139a020d69a19
IndraSubagja <69548809+IndraSubagja@users.noreply.github.com>
2021-09-09 17:25:17 +0700

    Update server.js

--- image-search-abstraction-layer/server.js
+++ image-search-abstraction-layer/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'Conncection error'));
+connection.on('error', console.error.bind(console, 'Connection error'));
 connection.once('open', () => {
   console.log('MongoDB database connected successfully');
 });
