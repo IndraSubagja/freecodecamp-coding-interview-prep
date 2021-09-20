8f7cd6dae763844ab8f4ebdb2c52b432b02ef515
IndraSubagja <69548809+IndraSubagja@users.noreply.github.com>
2021-09-21 04:31:44 +0700

    Update server.js

--- chart-the-stock-market/server.js
+++ chart-the-stock-market/server.js
@@ -5,53 +5,43 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 require('dotenv').config();
 
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => res.sendFile(process.cwd() + '/views/index.html'));
+
 let symbols = [];
 let datasets = [];
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-const getStock = async (symbol) => {
-  const { data } = await axios.get('https://www.alphavantage.co/query', {
-    params: {
-      function: 'TIME_SERIES_DAILY',
-      symbol,
-      apikey: process.env.API_KEY,
-    },
-  });
-
-  if (!data['Error Message']) {
-    datasets.push({
-      symbol: data['Meta Data']['2. Symbol'],
-      data: [
-        ...Object.entries(data['Time Series (Daily)']).map((entry) => [
-          new Date(entry[0]).getTime(),
-          parseFloat(entry[1]['4. close']),
-        ]),
-      ],
-    });
-
-    return data;
-  }
-};
-
-app.get('/', async (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-io.on('connection', async (socket) => {
+io.on('connection', (socket) => {
   socket.emit('chart', symbols, datasets);
 
   socket.on('getStock', async (symbol) => {
     if (!symbols.find((sym) => sym === symbol)) {
-      const data = await getStock(symbol, false);
+      const { data } = await axios.get('https://www.alphavantage.co/query', {
+        params: {
+          function: 'TIME_SERIES_DAILY',
+          symbol,
+          apikey: process.env.API_KEY,
+        },
+      });
 
-      if (data) {
+      if (!data['Error Message']) {
+        datasets.push({
+          symbol: data['Meta Data']['2. Symbol'],
+          data: [
+            ...Object.entries(data['Time Series (Daily)']).map((entry) => [
+              new Date(entry[0]).getTime(),
+              parseFloat(entry[1]['4. close']),
+            ]),
+          ],
+        });
+
         symbols.push(symbol);
         io.emit('chart', symbols, datasets);
       }
     }
   });
-  socket.on('removeStock', async (symbol) => {
+  socket.on('removeStock', (symbol) => {
     datasets = datasets.filter((dataset) => dataset.symbol !== symbol);
     symbols = symbols.filter((sym) => sym !== symbol);
     io.emit('chart', symbols, datasets);
